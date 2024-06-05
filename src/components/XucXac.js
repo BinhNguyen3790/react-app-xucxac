bbfb476ed7117d1ee0427a86e8799b51c886187a
llckumadevelop <kumauser01@kuma.ink>
2024-06-05 19:43:00 +0700

    add layout

---
+++ src/components/XucXac.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class XucXac extends Component {
+  render() {
+    return (
+      <div>
+        <img src="./assets/imgs/1.png" alt="" />
+        <img src="./assets/imgs/2.png" alt="" />
+        <img src="./assets/imgs/3.png" alt="" />
+      </div>
+    );
+  }
+}
+
+export default XucXac;
