4be413ed54cef485aad24721b031cc338e51a962
llckumadevelop <kumauser01@kuma.ink>
2024-06-08 19:51:54 +0700

    finish game

--- src/redux/GameReduce.js
+++ src/redux/GameReduce.js
@@ -17,6 +17,25 @@ const GameReduce = (state = stateDefault, action) => {
         return { ...state };
       }
       break;
+    case "PLAY_GAME":
+      {
+        let newMangXucXac = [];
+        for (let i = 0; i < 3; i++) {
+          let randomNumber = Math.floor(Math.random() * 6) + 1;
+          let randomXucXac = { id: randomNumber, img: `./assets/imgs/${randomNumber}.png` };
+          newMangXucXac.push(randomXucXac);
+        }
+        state.mangXucXac = newMangXucXac;
+        state.luotChoi += 1;
+        let tongDiem = newMangXucXac.reduce((tongDiem, xucxac, index) => {
+          return (tongDiem += xucxac.id);
+        }, 0);
+        if ((tongDiem > 11 && state.taiXiu === true) || (tongDiem < 11 && state.taiXiu === false)) {
+          state.soBanThang += 1;
+        }
+        return { ...state };
+      }
+      break;
     default:
       return { ...state };
   }
